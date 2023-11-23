89c9c14796f957a6d63a86745582db0e62ff3e9b
mccartnp <nicholasmccarty252@gmail.com>
2023-11-23 14:34:15 -0500

    General homepage layed out, refined visual representation

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'; 
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+    <div className="landing-page">
+      <h1>BIENVENIDOS a nicholasmccarty.dev</h1>
+      <p>Check out my socials and things that interest me.</p>
+      
+      <div className="social-links">
+        <a href="https://github.com/nicholaspmccarty" target="_blank" rel="noopener noreferrer">
+          <img src="https://cdn-icons-png.flaticon.com/512/25/25231.png" alt="GitHub Logo" className="social-icon" />
         </a>
-      </header>
+        <a href="https://www.instagram.com/nmccarty2" target="_blank" rel="noopener noreferrer">
+          <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/9/95/Instagram_logo_2022.svg/1200px-Instagram_logo_2022.svg.png" alt="Instagram Logo" className="social-icon" />
+        </a>
+      </div>
     </div>
   );
 }
